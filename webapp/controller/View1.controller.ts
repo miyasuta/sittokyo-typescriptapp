3b8a23cc0458688fd19ef9392dc90bad6c6ac266
Mio Yasutake <mio.fujita.01@gmail.com>
2024-02-16 12:34:54 +0900

    add comments

--- webapp/controller/View1.controller.ts
+++ webapp/controller/View1.controller.ts
@@ -6,18 +6,27 @@ import Controller from "sap/ui/core/mvc/Controller";
 export default class View1 extends Controller {
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
-    public onInit(): void {
+    public onInit(): void {       
         const recipient =  {
             recipientName : "World"
         };
+        // JSONモデルを作成
+
+        // Viewに登録
     }
 
     public onShowHello() {
-
+        // ViewModelからrecipientNameを取得
+        
+        // MessageBoxで表示
+        // MessageBox.show(`Hello ${name}`);
     }
 
     public onLiveChange() {
+        // イベントパラメータ：valueを取得
 
+        // MessageToastで表示
+        // MessageToast.show(`Value changed to ${value}`);
     }
 
 
