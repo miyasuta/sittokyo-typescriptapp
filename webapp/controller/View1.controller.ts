ce2d4e2990906fdacb8c054e39201487dae0f739
Mio Yasutake <mio.fujita.01@gmail.com>
2024-02-16 11:57:26 +0900

    add skeleton

--- webapp/controller/View1.controller.ts
+++ webapp/controller/View1.controller.ts
@@ -7,6 +7,18 @@ export default class View1 extends Controller {
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
+        const recipient =  {
+            recipientName : "World"
+        };
+    }
+
+    public onShowHello() {
 
     }
+
+    public onLiveChange() {
+
+    }
+
+
 }
